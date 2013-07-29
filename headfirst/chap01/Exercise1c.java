17d407cf808147e16d659ee0b585b120ef903de0
Andrew Newton <andrew@SentinelHome.(none)>
2013-07-29 22:52:37 +0100

    new file:   headfirst/chap01/Exercise1b.class 	new file:   headfirst/chap01/Exercise1b.java 	new file:   headfirst/chap01/Exercise1b.java~ 	new file:   headfirst/chap01/Exercise1c.class 	new file:   headfirst/chap01/Exercise1c.java 	new file:   headfirst/chap01/Exercise1c.java~ 	new file:   headfirst/chap01/PoolPuzzleOne.class 	new file:   headfirst/chap01/PoolPuzzleOne.java 	new file:   headfirst/chap01/PoolPuzzleOne.java~ 	new file:   headfirst/chap01/Shuffle1.class 	new file:   headfirst/chap01/Shuffle1.java 	new file:   headfirst/chap01/Shuffle1.java~ 	new file:   headfirst/chap01/Shuffle2.java 	new file:   headfirst/chap01/Shuffle2.java~ 	new file:   headfirst/chap01/shuffle2.class 	new file:   headfirst/chap02/DrumKit.class 	new file:   headfirst/chap02/DrumKitTestDrive.class 	new file:   headfirst/chap02/DrumKitTestDrive.java 	new file:   headfirst/chap02/DrumKitTestDrive.java~ 	new file:   headfirst/chap02/Echo.class 	new file:   headfirst/chap02/EchoTestDrive.class 	new file:   headfirst/chap02/EchoTestDrive.java 	new file:   headfirst/chap02/EchoTestDrive.java~ 	new file:   headfirst/chap03/Mix4.java 	new file:   headfirst/chap03/Mix4.java~ 	new file:   headfirst/chap03/TestArrays.class 	new file:   headfirst/chap03/TestArrays.java 	new file:   headfirst/chap03/TestArrays.java~ 	new file:   headfirst/chap03/Triangle.class 	new file:   headfirst/chap03/Triangle.java 	new file:   headfirst/chap03/Triangle.java~ 	new file:   headfirst/chap04/Mix4.class 	new file:   headfirst/chap04/Mix4.java 	new file:   headfirst/chap04/Puzzle4.class 	new file:   headfirst/chap04/Puzzle4.java 	new file:   headfirst/chap04/Puzzle4.java~ 	new file:   headfirst/chap04/Puzzle4b.class

---
+++ headfirst/chap01/Exercise1c.java
@@ -0,0 +1,13 @@
+class Exercise1 {
+
+	public static void main(String[] arg) {
+		int x =5; 
+		
+		while (x >1 ) {
+			x = x-1;
+			if ( x < 3 ) {
+				System.out.println("small x"); 
+			}
+		}
+	}
+}
